6c3a7428d9ca0787a4a8de102cd4df21ca927fe6
IlonaChupa <ilonachupa@gmail.com>
2026-01-31 14:39:39 +0200

    changesssss

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public abstract class Figure implements Drawable , HasArea {
+public abstract class Figure implements Drawable, HasArea {
     protected String color;
 
     public Figure(String color) {
